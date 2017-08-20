71dadebd07345e06daae70d05c45a9588ccf49a6
Eugene Valeyev <evgen.povt@gmail.com>
2017-08-20 20:57:27 +0300

    Visibility control on create will page

--- assets/js/create-will.js
+++ assets/js/create-will.js
@@ -1,5 +1,13 @@
 $('#submit-email').click( (e) => {
+  $('#submitted-email-successful').removeAttr('hidden');
+  $('#confirmation-section').removeAttr('hidden');
 });
 
 $('#confirm-email').click( (e) => {
+  const redirect = 'http://localhost:8080/public/will-create.html';
+
+  setTimeout( () => window.location = redirect, 3000);
+
+  $('#redirect-link').attr('href', redirect);
+  $('#confirmed-email-successful').removeAttr('hidden');
 });
