8f5b14e672be431f1abaa5454d0bda2cf3601d5e
Eugene Valeyev <evgen.povt@gmail.com>
2018-08-25 16:56:45 +0300

    Fixed getting an address public key

--- api/controllers/KeyController.js
+++ api/controllers/KeyController.js
@@ -6,6 +6,9 @@
  */
 
 const request = require('request');
+const eu = require('ethereumjs-util');
+const RLP = require("eth-lib/lib/rlp");
+const Bytes = require("eth-lib/lib/bytes");
 
 function requestEtherscan(query) {
   const apiKey = sails.config.custom.etherscan.apiKey;
@@ -24,20 +27,50 @@ function requestEtherscan(query) {
 module.exports = {
   
   find: (req, res) => {
-    const address = req.query.address;
+    const address = req.query.address.toLowerCase();
 
     const query = `module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc`;
     requestEtherscan(query).then( (response) => {
       //todo: if response.status != 1 || !response.result || response.result.length == 0 throw
       const txs = response.result;
-      const txId = txs[0].hash;
+      const outTx = txs.find( tx => tx.from.toLowerCase() === address );
+      if (!outTx) return Promise.reject('Not found'); //todo; make a correct error
+      const txId = outTx.hash;
       const query = `module=proxy&action=eth_getTransactionByHash&txhash=${txId}`;
       return requestEtherscan(query);
     }).then( (response) => {
       const tx = response.result;
+      const rlpEncoded = RLP.encode([
+        Bytes.fromNat(tx.nonce),
+        Bytes.fromNat(tx.gasPrice),
+        Bytes.fromNat(tx.gas),
+        tx.to.toLowerCase(),
+        Bytes.fromNat(tx.value),
+        tx.input,
+        Bytes.fromNat(tx.chainId || "0x1"),
+        "0x",
+        "0x"
+      ]);
+
+      const signatureV = (rawV) => {
+        let v = rawV;
+        if (rawV > 35) v = rawV - 10;
+        return v;
+      };
+
+      const rawTxHash = eu.keccak(rlpEncoded);
+      const euPubKey = eu.ecrecover(rawTxHash, signatureV(tx.v), tx.r, tx.s);
+      const addr = '0x' + eu.pubToAddress(euPubKey).toString('hex');
+      sails.log('address: ' + addr);
+      sails.log('pub key: ' + euPubKey.toString('hex'));
+
+      if (addr !== address) {
+        return Promise.reject('Wrong address'); //todo: error
+      }
+
       return res.ok({
-        address: address,
-        publicKey: '0x04' + tx.publicKey.slice(2)
+        address: addr,
+        publicKey: '0x04' + euPubKey.toString('hex')
       });
     }).catch( (error) => {
       return res.serverError(error);
